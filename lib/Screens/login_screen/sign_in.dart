68f4582a54de931164b2509f9f795fbbb7a9f089
Sreenath Gopinathan <sreenath4294@gmail.com>
2022-10-29 20:06:23 +0530

    update Signin page

--- lib/Screens/login_screen/sign_in.dart
+++ lib/Screens/login_screen/sign_in.dart
@@ -20,149 +20,156 @@ class _SignInPageState extends State<SignInPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(25.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 60),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          // color: Colors.deepPurple,
-                          ),
-                      padding: const EdgeInsets.all(50),
-                      // child: const Text("IMG")
-                      child: Image.asset(
-                        "assets/logo.png",
-                        height: 224,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextFormField(
-                      controller: emailText,
-                      decoration: const InputDecoration(
-                        labelText: 'Email',
-                        hintText: 'Enter Email',
-                        floatingLabelStyle: TextStyle(color: Colors.deepPurple),
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.deepPurple, width: 2),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            // color: Colors.deepPurple,
+                            ),
+                        padding: const EdgeInsets.all(50),
+                        // child: const Text("IMG")
+                        child: Image.asset(
+                          "assets/logo.png",
+                          height: 224,
                         ),
                       ),
-                      style: const TextStyle(fontSize: 13),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Email cannot be empty';
-                        }
-                        return null;
-                      },
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      controller: passwordText,
-                      decoration: const InputDecoration(
-                        labelText: 'Password',
-                        hintText: 'Enter Password',
-                        floatingLabelStyle: TextStyle(color: Colors.deepPurple),
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.deepPurple, width: 2),
+                  ],
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: emailText,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter Email',
+                          floatingLabelStyle:
+                              TextStyle(color: Colors.deepPurple),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.deepPurple, width: 2),
+                          ),
                         ),
+                        style: const TextStyle(fontSize: 13),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Email cannot be empty';
+                          }
+                          return null;
+                        },
                       ),
-                      style: const TextStyle(fontSize: 13),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Password cannot be empty';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius: BorderRadius.circular(5)),
-                      width: double.infinity,
-                      child: TextButton(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              if (emailText.text == email &&
-                                  passwordText.text == password) {
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        controller: passwordText,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter Password',
+                          floatingLabelStyle:
+                              TextStyle(color: Colors.deepPurple),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.deepPurple, width: 2),
+                          ),
+                        ),
+                        style: const TextStyle(fontSize: 13),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Password cannot be empty';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(5)),
+                        width: double.infinity,
+                        child: TextButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                if (emailText.text == email &&
+                                    passwordText.text == password) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Signing you in')),
+                                  );
+                                }
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   const SnackBar(
-                                      content: Text('Signing you in')),
+                                      content: Text('Wrong Credentials')),
                                 );
                               }
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text('Wrong Credentials')),
-                              );
-                            }
-                          },
+                            },
+                            child: const Text(
+                              "Sign in",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "OR",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(5)),
+                        width: double.infinity,
+                        child: TextButton(
+                          onPressed: () => "success",
                           child: const Text(
-                            "Sign in",
+                            "Register",
                             style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 15,
                                 fontWeight: FontWeight.bold),
-                          )),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      "OR",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius: BorderRadius.circular(5)),
-                      width: double.infinity,
-                      child: TextButton(
-                        onPressed: () => "success",
-                        child: const Text(
-                          "Register",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Text(
-                      "Forgot password?",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                      const SizedBox(
+                        height: 20,
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                      const Text(
+                        "Forgot password?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
